08e376cbb4ac704ea136afc4ce5efbd4f3049dc5
Ertuğrul Şengül <ertugrul.sengul00@gmail.com>
2023-02-28 00:49:56 +0300

    Login Screens Completed

---
+++ app/src/main/java/com/ertu_/myhome/login/SignInActivity.java
@@ -0,0 +1,21 @@
+package com.ertu_.myhome.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.ertu_.myhome.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+
+    public void enter(View view) {
+
+    }
+}
---
+++ app/src/main/java/com/ertu_/myhome/login/SignUpActivity.java
@@ -0,0 +1,22 @@
+package com.ertu_.myhome.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.ertu_.myhome.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+    public void sign_up_click(View view) {
+
+    }
+
+}
