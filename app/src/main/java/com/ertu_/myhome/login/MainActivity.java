c9affa85ea7f52dab4cd5a53d1f3061d66f5059c
Ertuğrul Şengül <ertugrul.sengul00@gmail.com>
2023-03-03 17:49:29 +0300

    google sign in

--- app/src/main/java/com/ertu_/myhome/login/MainActivity.java
+++ app/src/main/java/com/ertu_/myhome/login/MainActivity.java
@@ -7,15 +7,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.ertu_.myhome.R;
+import com.ertu_.myhome.databinding.ActivityMainBinding;
 import com.ertu_.myhome.home.HomeActivity;
+import com.ertu_.myhome.login.socialAccLogin.GoogleSignInLogin;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+
+        binding.googleImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GoogleSignInLogin.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null)
+            reload(MainActivity.this);
     }
 
     public void sign_in(View view) {
@@ -34,5 +61,8 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
+    private void google() {
+
+    }
 
 }
